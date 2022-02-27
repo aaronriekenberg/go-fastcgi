1dac69bf1f3caa917b18a44ae35a49fda6beb585
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2022-02-27 16:50:41 -0600

    Add command api handler.

--- handlers/command/command.go
+++ handlers/command/command.go
@@ -2,10 +2,13 @@ package command
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os/exec"
 	"time"
 
 	"github.com/aaronriekenberg/go-fastcgi/config"
@@ -33,16 +36,15 @@ func CreateCommandHandler(configuration *config.Configuration, serveMux *http.Se
 		commandHandler.getAllCommandsHandlerFunc(commandConfiguration),
 	)
 
-	// for _, commandInfo := range commandConfiguration.Commands {
-	// 	apiPath := "/cgi-bin/commands/" + commandInfo.ID
-	// 	serveMux.Handle(
-	// 		apiPath,
-	// 		commandHandler.commandAPIHandlerFunc(commandInfo))
-	// }
+	for _, commandInfo := range commandConfiguration.Commands {
+		apiPath := "/cgi-bin/commands/" + commandInfo.ID
+		serveMux.Handle(
+			apiPath,
+			commandHandler.commandAPIHandlerFunc(commandInfo))
+	}
 }
 
 func (commandHandler *commandHandler) getAllCommandsHandlerFunc(commandConfiguration *config.CommandConfiguration) http.HandlerFunc {
-
 	jsonBuffer, err := json.Marshal(commandConfiguration.Commands)
 	if err != nil {
 		log.Fatalf("getAllCommandsHandlerFunc json.Marshal err = %v", err)
@@ -52,5 +54,82 @@ func (commandHandler *commandHandler) getAllCommandsHandlerFunc(commandConfigura
 		w.Header().Add(utils.ContentTypeHeaderKey, utils.ContentTypeApplicationJSON)
 		w.Header().Add(utils.CacheControlHeaderKey, utils.MaxAgeZero)
 		io.Copy(w, bytes.NewReader(jsonBuffer))
+	}
+}
+
+func (commandHandler *commandHandler) acquireCommandSemaphore(ctx context.Context) (err error) {
+	ctx, cancel := context.WithTimeout(ctx, commandHandler.semaphoreAcquireTimeout)
+	defer cancel()
+
+	err = commandHandler.commandSemaphore.Acquire(ctx, 1)
+	if err != nil {
+		err = fmt.Errorf("commandHandler.acquireCommandSemaphore error calling Acquire: %w", err)
+	}
+	return
+}
+
+func (commandHandler *commandHandler) releaseCommandSemaphore() {
+	commandHandler.commandSemaphore.Release(1)
+}
+
+type commandAPIResponse struct {
+	CommandInfo     *config.CommandInfo `json:"commandInfo"`
+	Now             string              `json:"now"`
+	CommandDuration string              `json:"commandDuration"`
+	CommandOutput   string              `json:"commandOutput"`
+}
+
+func (commandHandler *commandHandler) runCommand(ctx context.Context, commandInfo *config.CommandInfo) (response *commandAPIResponse) {
+	err := commandHandler.acquireCommandSemaphore(ctx)
+	if err != nil {
+		response = &commandAPIResponse{
+			CommandInfo:   commandInfo,
+			Now:           utils.FormatTime(time.Now()),
+			CommandOutput: fmt.Sprintf("%v", err),
+		}
+		return
+	}
+	defer commandHandler.releaseCommandSemaphore()
+
+	commandStartTime := time.Now()
+	rawCommandOutput, err := exec.CommandContext(
+		ctx, commandInfo.Command, commandInfo.Args...).CombinedOutput()
+	commandEndTime := time.Now()
+
+	var commandOutput string
+	if err != nil {
+		commandOutput = fmt.Sprintf("command error %v", err)
+	} else {
+		commandOutput = string(rawCommandOutput)
+	}
+
+	commandDuration := fmt.Sprintf("%.9f sec",
+		commandEndTime.Sub(commandStartTime).Seconds())
+
+	response = &commandAPIResponse{
+		CommandInfo:     commandInfo,
+		Now:             utils.FormatTime(commandEndTime),
+		CommandDuration: commandDuration,
+		CommandOutput:   commandOutput,
+	}
+	return
+}
+func (commandHandler *commandHandler) commandAPIHandlerFunc(commandInfo config.CommandInfo) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx, cancel := context.WithTimeout(context.Background(), commandHandler.requestTimeout)
+		defer cancel()
+
+		commandAPIResponse := commandHandler.runCommand(ctx, &commandInfo)
+
+		var jsonText []byte
+		jsonText, err := json.Marshal(commandAPIResponse)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Add(utils.ContentTypeHeaderKey, utils.ContentTypeApplicationJSON)
+		w.Header().Add(utils.CacheControlHeaderKey, utils.MaxAgeZero)
+		io.Copy(w, bytes.NewReader(jsonText))
 	}
 }
--- utils/utils.go
+++ utils/utils.go
@@ -1,8 +1,16 @@
 package utils
 
+import "time"
+
 const (
 	CacheControlHeaderKey      = "cache-control"
 	MaxAgeZero                 = "max-age=0"
 	ContentTypeHeaderKey       = "content-type"
 	ContentTypeApplicationJSON = "application/json"
 )
+
+func FormatTime(t time.Time) string {
+	const timeFormat = "Mon Jan 2 15:04:05.000000000 -0700 MST 2006"
+
+	return t.Format(timeFormat)
+}
