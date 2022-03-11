41d243f290f459725ec56cc0d2687c4701c25f1f
Aaron Riekenberg <aaron.riekenberg@target.com>
2022-03-11 15:12:42 -0600

    Make request_info return a json response with individual fields.

--- handlers/debug/debug.go
+++ handlers/debug/debug.go
@@ -3,83 +3,45 @@ package debug
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"io"
 	"net/http"
-	"sort"
-	"strings"
 
 	"github.com/aaronriekenberg/go-fastcgi/config"
 	"github.com/aaronriekenberg/go-fastcgi/utils"
 )
 
-func httpHeaderToString(header http.Header) string {
-	var builder strings.Builder
-	keys := make([]string, 0, len(header))
-	for key := range header {
-		keys = append(keys, key)
-	}
-	sort.Strings(keys)
-	for i, key := range keys {
-		if i != 0 {
-			builder.WriteRune('\n')
-		}
-		builder.WriteString(key)
-		builder.WriteString(": ")
-		fmt.Fprintf(&builder, "%v", header[key])
-	}
-	return builder.String()
-}
-
 type requestInfoData struct {
-	RequestInfo string `json:"requestInfo"`
+	HTTPMethod        string      `json:"httpMethod"`
+	HTTPProtocol      string      `json:"httpProtocol"`
+	Host              string      `json:"host"`
+	RemoteAddress     string      `json:"remoteAddress"`
+	RequestURI        string      `json:"requestURI"`
+	URL               string      `json:"url"`
+	BodyContentLength int64       `json:"bodyContentLength"`
+	Close             bool        `json:"close"`
+	RequestHeaders    http.Header `json:"requestHeaders"`
 }
 
 func requestInfoHandlerFunc() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var buffer strings.Builder
 
-		buffer.WriteString("Method: ")
-		buffer.WriteString(r.Method)
-		buffer.WriteRune('\n')
-
-		buffer.WriteString("Protocol: ")
-		buffer.WriteString(r.Proto)
-		buffer.WriteRune('\n')
-
-		buffer.WriteString("Host: ")
-		buffer.WriteString(r.Host)
-		buffer.WriteRune('\n')
-
-		buffer.WriteString("RemoteAddr: ")
-		buffer.WriteString(r.RemoteAddr)
-		buffer.WriteRune('\n')
-
-		buffer.WriteString("RequestURI: ")
-		buffer.WriteString(r.RequestURI)
-		buffer.WriteRune('\n')
-
-		buffer.WriteString("URL: ")
-		fmt.Fprintf(&buffer, "%#v", r.URL)
-		buffer.WriteRune('\n')
-
-		buffer.WriteString("Body.ContentLength: ")
-		fmt.Fprintf(&buffer, "%v", r.ContentLength)
-		buffer.WriteRune('\n')
-
-		buffer.WriteString("Close: ")
-		fmt.Fprintf(&buffer, "%v", r.Close)
-		buffer.WriteRune('\n')
-
-		buffer.WriteString("TLS: ")
-		fmt.Fprintf(&buffer, "%#v", r.TLS)
-		buffer.WriteString("\n\n")
-
-		buffer.WriteString("Request Headers:\n")
-		buffer.WriteString(httpHeaderToString(r.Header))
+		var urlString string
+		if r.URL != nil {
+			urlString = r.URL.String()
+		} else {
+			urlString = "(null)"
+		}
 
 		response := &requestInfoData{
-			RequestInfo: buffer.String(),
+			HTTPMethod:        r.Method,
+			HTTPProtocol:      r.Proto,
+			Host:              r.Host,
+			RemoteAddress:     r.RemoteAddr,
+			RequestURI:        r.RequestURI,
+			URL:               urlString,
+			BodyContentLength: r.ContentLength,
+			Close:             r.Close,
+			RequestHeaders:    r.Header,
 		}
 
 		jsonText, err := json.Marshal(response)
