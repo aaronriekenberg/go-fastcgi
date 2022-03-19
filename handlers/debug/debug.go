25ea396956328e6e432f2bf2d16a285d5ac1059c
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2022-03-19 09:59:18 -0500

    split into single and multi value request headers.

--- handlers/debug/debug.go
+++ handlers/debug/debug.go
@@ -11,30 +11,40 @@ import (
 )
 
 type requestInfoData struct {
-	HTTPMethod        string            `json:"httpMethod"`
-	HTTPProtocol      string            `json:"httpProtocol"`
-	Host              string            `json:"host"`
-	RemoteAddress     string            `json:"remoteAddress"`
-	URL               string            `json:"url"`
-	BodyContentLength int64             `json:"bodyContentLength"`
-	Close             bool              `json:"close"`
-	RequestHeaders    map[string]string `json:"requestHeaders"`
+	HTTPMethod                string              `json:"httpMethod"`
+	HTTPProtocol              string              `json:"httpProtocol"`
+	Host                      string              `json:"host"`
+	RemoteAddress             string              `json:"remoteAddress"`
+	URL                       string              `json:"url"`
+	BodyContentLength         int64               `json:"bodyContentLength"`
+	Close                     bool                `json:"close"`
+	SingleValueRequestHeaders map[string]string   `json:"singleValueRequestHeaders"`
+	MultiValueRequestHeaders  map[string][]string `json:"multiValueRequestHeaders"`
 }
 
 func httpHeaderToSingleValueMap(headers http.Header) map[string]string {
 	retVal := make(map[string]string)
 
 	for key, value := range headers {
-		if len(value) > 0 {
+		if len(value) == 1 {
 			retVal[key] = value[0]
-		} else {
-			retVal[key] = ""
 		}
 	}
 
 	return retVal
 }
 
+func httpHeaderToMultiValueMap(headers http.Header) map[string][]string {
+	retVal := make(map[string][]string)
+
+	for key, value := range headers {
+		if len(value) != 1 {
+			retVal[key] = value
+		}
+	}
+
+	return retVal
+}
 func requestInfoHandlerFunc() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
@@ -46,14 +56,15 @@ func requestInfoHandlerFunc() http.HandlerFunc {
 		}
 
 		response := &requestInfoData{
-			HTTPMethod:        r.Method,
-			HTTPProtocol:      r.Proto,
-			Host:              r.Host,
-			RemoteAddress:     r.RemoteAddr,
-			URL:               urlString,
-			BodyContentLength: r.ContentLength,
-			Close:             r.Close,
-			RequestHeaders:    httpHeaderToSingleValueMap(r.Header),
+			HTTPMethod:                r.Method,
+			HTTPProtocol:              r.Proto,
+			Host:                      r.Host,
+			RemoteAddress:             r.RemoteAddr,
+			URL:                       urlString,
+			BodyContentLength:         r.ContentLength,
+			Close:                     r.Close,
+			SingleValueRequestHeaders: httpHeaderToSingleValueMap(r.Header),
+			MultiValueRequestHeaders:  httpHeaderToMultiValueMap(r.Header),
 		}
 
 		jsonText, err := json.Marshal(response)
