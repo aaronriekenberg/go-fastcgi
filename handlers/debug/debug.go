f8b744da6cc82c7228c0ac7bcdac419e4e005d92
Aaron Riekenberg <aaron.riekenberg@target.com>
2022-04-01 16:52:29 -0500

    Add requestHeaders struct.

--- handlers/debug/debug.go
+++ handlers/debug/debug.go
@@ -10,41 +10,40 @@ import (
 	"github.com/aaronriekenberg/go-fastcgi/utils"
 )
 
-type requestInfoData struct {
-	HTTPMethod                string              `json:"httpMethod"`
-	HTTPProtocol              string              `json:"httpProtocol"`
-	Host                      string              `json:"host"`
-	RemoteAddress             string              `json:"remoteAddress"`
-	URL                       string              `json:"url"`
-	BodyContentLength         int64               `json:"bodyContentLength"`
-	Close                     bool                `json:"close"`
-	SingleValueRequestHeaders map[string]string   `json:"singleValueRequestHeaders"`
-	MultiValueRequestHeaders  map[string][]string `json:"multiValueRequestHeaders"`
+type requestHeaders struct {
+	SingleValue map[string]string   `json:"singleValue"`
+	MultiValue  map[string][]string `json:"multiValue"`
 }
 
-func httpHeaderToSingleValueMap(headers http.Header) map[string]string {
-	retVal := make(map[string]string)
+type requestInfoData struct {
+	HTTPMethod        string         `json:"httpMethod"`
+	HTTPProtocol      string         `json:"httpProtocol"`
+	Host              string         `json:"host"`
+	RemoteAddress     string         `json:"remoteAddress"`
+	URL               string         `json:"url"`
+	BodyContentLength int64          `json:"bodyContentLength"`
+	Close             bool           `json:"close"`
+	RequestHeaders    requestHeaders `json:"requestHeaders"`
+}
+
+func httpHeaderToRequestHeaders(headers http.Header) requestHeaders {
+
+	requestHeaders := requestHeaders{
+		SingleValue: make(map[string]string),
+		MultiValue:  make(map[string][]string),
+	}
 
 	for key, value := range headers {
 		if len(value) == 1 {
-			retVal[key] = value[0]
+			requestHeaders.SingleValue[key] = value[0]
+		} else {
+			requestHeaders.MultiValue[key] = value
 		}
 	}
 
-	return retVal
+	return requestHeaders
 }
 
-func httpHeaderToMultiValueMap(headers http.Header) map[string][]string {
-	retVal := make(map[string][]string)
-
-	for key, value := range headers {
-		if len(value) != 1 {
-			retVal[key] = value
-		}
-	}
-
-	return retVal
-}
 func requestInfoHandlerFunc() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
@@ -56,15 +55,14 @@ func requestInfoHandlerFunc() http.HandlerFunc {
 		}
 
 		response := &requestInfoData{
-			HTTPMethod:                r.Method,
-			HTTPProtocol:              r.Proto,
-			Host:                      r.Host,
-			RemoteAddress:             r.RemoteAddr,
-			URL:                       urlString,
-			BodyContentLength:         r.ContentLength,
-			Close:                     r.Close,
-			SingleValueRequestHeaders: httpHeaderToSingleValueMap(r.Header),
-			MultiValueRequestHeaders:  httpHeaderToMultiValueMap(r.Header),
+			HTTPMethod:        r.Method,
+			HTTPProtocol:      r.Proto,
+			Host:              r.Host,
+			RemoteAddress:     r.RemoteAddr,
+			URL:               urlString,
+			BodyContentLength: r.ContentLength,
+			Close:             r.Close,
+			RequestHeaders:    httpHeaderToRequestHeaders(r.Header),
 		}
 
 		jsonText, err := json.Marshal(response)
